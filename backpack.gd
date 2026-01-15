312568661083c9af8413bf41ce44526a6865f9d6
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-01-14 22:15:47 -0500

    Distinguish regular items and weapons

--- backpack.gd
+++ backpack.gd
@@ -1,16 +1,32 @@
 extends Node3D
 
 @export var item_slots: Array[Node3D]
-#@export var weapon_slots: Array[Node3D]
+@export var weapon_slots: Array[Node3D]
 
 func attempt_store_item(item: Item):
 	
-	for item_slot in item_slots:
+	if item.is_weapon:
 		
-		if item_slot.get_child_count() == 0:
-			item.freeze = true
-			item.get_child(0).disabled = true
-			item.reparent(item_slot)
-			item.position = Vector3.ZERO
-			item.rotation = Vector3(0, -PI / 2, 0)
-			break
+		for slot in weapon_slots:
+			
+			if slot.get_child_count() == 0:
+				
+				item.freeze = true
+				item.get_child(0).disabled = true
+				item.reparent(slot)
+				item.position = Vector3.ZERO
+				item.rotation = Vector3(0, -PI / 2, 0)
+				break
+		
+	else:
+	
+		for slot in item_slots:
+			
+			if slot.get_child_count() == 0:
+				
+				item.freeze = true
+				item.get_child(0).disabled = true
+				item.reparent(slot)
+				item.position = Vector3.ZERO
+				item.rotation = Vector3(0, -PI / 2, 0)
+				break
--- item.gd
+++ item.gd
@@ -1,2 +1,4 @@
 extends RigidBody3D
 class_name Item
+
+@export var is_weapon: bool = false
