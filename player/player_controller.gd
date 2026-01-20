0901c011aea54883b4cb0e47a6f028acd83804d1
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-01-19 22:31:59 -0500

    Slight refactor

--- player/player_controller.gd
+++ player/player_controller.gd
@@ -148,7 +148,7 @@ func _input(event):
 			
 			if look_item:
 				look_item.set_rigidbody(true)
-				look_item.reparent(get_tree().root.get_child(0))
+				look_item.reparent(ROOT_NODE)
 	
 	elif event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		
